1d7e5b0e397b7f41abcb5247dc1f087389b535b1
slopezvvv <slopez@tuserver.cl>
2019-11-25 13:50:44 +0000

    Initial commit

---
+++ app/src/main/java/cl/tuserver/clase_firebase/Login.java
@@ -0,0 +1,56 @@
+package cl.tuserver.clase_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    private EditText correo, pass;
+    private Button ingresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        correo = findViewById(R.id.txtCorreo);
+        pass = findViewById(R.id.txtPassword);
+        ingresar = findViewById(R.id.btnLogin);
+
+        ingresar.setOnClickListener(v -> procesoDeLogin(
+                correo.getText().toString(),
+                pass.getText().toString()
+        ));
+    }
+
+    private void procesoDeLogin(String correo, String pass){
+        if(correo.isEmpty()){
+            // mandar un mensaje
+            Toast.makeText(this, getString(R.string.errorLogin), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (pass.isEmpty()){
+            // mandar un mensaje
+            Toast.makeText(this, getString(R.string.errorLogin), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(correo, pass)
+        .addOnSuccessListener(auth -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        })
+        .addOnFailureListener(auth -> {
+            Toast.makeText(this, getString(R.string.errorLogin), Toast.LENGTH_SHORT).show();
+            System.out.println("ERROR_AUTH_FIREBASE: "+auth.getMessage());
+        });
+    }
+}
---
+++ app/src/main/java/cl/tuserver/clase_firebase/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.tuserver.clase_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
